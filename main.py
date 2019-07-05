78c154653c2da6ceb82c7351170015188c9cad53
carloosamarante <amarante.carlos@hotmail.com>
2019-07-05 10:41:49 -0300

    Organize the code, using methods

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-string = "Tamo junto"
-encrypted_string_input = "Vcoqlwpvq"
-key = 2
-
-def encrypt(string, key):
+def encrypt(normal_string, key):
     encrypted_string_output = []
-    for char in string:
+    for char in normal_string:
         encrypted_char = chr(ord(char) + key)
         encrypted_string_output.append(encrypted_char)
     print(''.join(encrypted_string_output))
 
-
-
-def decrypt(string, key):
+def decrypt(encrypted_string, key):
     decrypted_string_output = []
-    for char in encrypted_string_input:
+    for char in encrypted_string:
         decrypted_char = chr(ord(char) - key)
         decrypted_string_output.append(decrypted_char)
     print(''.join(decrypted_string_output))
 
+def operation_type(op, string, key):
+    switcher = {
+        1: encrypt,
+        2: decrypt
+    }
+    main_function = switcher.get(op, "Invalid operation")
+    return main_function(string, key)
 
-encrypt(string, key)
+print("Select one option bellow: (1 for Encrypt and 2 for Decrypt)")
+print("1. Encrypt          2. Decrypt")
+code_type = int(input())
+print(code_type)
+print("Input your string:")
+string = input()
+print("Input the key:")
+key = int(input())
+
+operation_type(code_type, string, key)
